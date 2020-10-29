0b1d02f1d4d7ca5e48a87efd1df413b69f72381d
NoFunny <chunikhin_1999@mail.ru>
2020-10-30 02:09:18 +0700

    Add condition for actions

--- lib/basic/loop.rb
+++ lib/basic/loop.rb
@@ -6,11 +6,11 @@ class Loop
       menu.draw(human)
 
       case gets.chomp
-      when '1' then human.go_to_work
+      when '1' then human.go_to_work if human.mana < 50 && human.tired > 10
       when '2' then human.contemplate_nature
-      when '3' then human.drink_wine_and_watch_tv_series
-      when '4' then human.go_to_the_bar
-      when '5' then human.drink_with_marginalized_people
+      when '3' then human.drink_wine_and_watch_tv_series if human.money >= 20
+      when '4' then human.go_to_the_bar if human.money >= 100
+      when '5' then human.drink_with_marginalized_people if human.money >= 150
       when '6' then human.sing_in_the_subway
       when '7' then human.sleep
       when '8' then save_progress(human)
