afc2cdd541656c82d1256ad41975d46a898cf392
ZinkPavel <tsynk.p@gmail.com>
2020-11-02 18:44:35 +0700

    Fix serialize_human func

--- lib/io/serialize.rb
+++ lib/io/serialize.rb
@@ -1,11 +1,11 @@
 def serialize_human(human)
   {
     'name' => human.name,
-    'alive' => human.alive,
     'health' => human.health,
     'mana' => human.mana,
     'happiness' => human.happiness,
     'tired' => human.tired,
-    'money' => human.money
+    'money' => human.money,
+    'alive' => human.alive
   }.to_json
 end
