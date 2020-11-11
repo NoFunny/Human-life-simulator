73b3e406bc9e6ae96fb56d0dff1bde8832e9c619
ZinkPavel <tsynk.p@gmail.com>
2020-11-11 09:58:42 +0700

    Add selecting saves

--- lib/io/io_adapter.rb
+++ lib/io/io_adapter.rb
@@ -10,6 +10,20 @@ class IOAdapter
   end
 
   def load_progress(human)
-    deserialize_human(human, File.open('resources/data.json').readline)
+    saves = available_saves
+    puts 'No saves available' if saves.length.zero?
+    counter = 0
+
+    puts "\nSelect save:"
+    saves.each do |save|
+      puts "#{counter += 1} - #{save}"
+    end
+
+    choice = gets.chomp.to_i - 1
+    deserialize_human(human, File.open(saves[choice]).readline)
+  end
+
+  def available_saves
+    Dir['resources/*.json']
   end
 end
