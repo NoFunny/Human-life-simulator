875058b084e4d2bd2ad160872cbec71bb0fc62f1
ZinkPavel <tsynk.p@gmail.com>
2020-11-11 07:59:40 +0700

    Move functions to class

--- lib/io/io_adapter.rb
+++ lib/io/io_adapter.rb
@@ -2,14 +2,16 @@ require_relative '../basic/human'
 require_relative 'serialize'
 require_relative 'deserialize'
 
-def save_progress(human)
-  File.open('resources/data.json', 'w') do |file|
-    file.print serialize_human(human)
+class IOAdapter
+  def save_progress(human)
+    File.open('resources/data.json', 'w') do |file|
+      file.print serialize_human(human)
+    end
   end
-end
 
-def load_progress(checks = Inspector.new)
-  instance = deserialize_human(File.open('resources/data.json').readline)
-  checks.human = instance
-  instance
+  def load_progress(checks = Inspector.new)
+    instance = deserialize_human(File.open('resources/data.json').readline)
+    checks.human = instance
+    instance
+  end
 end
