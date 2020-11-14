128e13e2fea0be725fbcdb25e6981aa6103babde
ZinkPavel <tsynk.p@gmail.com>
2020-11-14 17:16:10 +0700

    Write tests by better specs

--- spec/basic/cause_spec.rb
+++ spec/basic/cause_spec.rb
@@ -6,33 +6,57 @@ describe 'Cause' do
   let(:cause) { Cause.new }
   let(:human) { Human.new }
 
-  it '.less_health' do
-    human.health = 0
-    expect(cause.less_health(human)).to eq("#{human.name.chomp} не подлечил здоровье и скончался.\n")
+  describe '.less_health' do
+    subject do
+      human.health = 0
+      cause.less_health(human)
+    end
+
+    it { is_expected.to eq("#{human.name.chomp} не подлечил здоровье и скончался.\n") }
   end
 
-  it '.very_tired' do
-    human.tired = 100
-    expect(cause.very_tired(human)).to eq("#{human.name.chomp} не жалел себя и устал досмерти.\n")
+  describe '.very_tired' do
+    subject do
+      human.tired = 100
+      cause.very_tired(human)
+    end
+
+    it { is_expected.to eq("#{human.name.chomp} не жалел себя и устал досмерти.\n") }
   end
 
-  it '.a_lot_of_mana' do
-    human.mana = 100
-    expect(cause.a_lot_of_mana(human)).to eq("#{human.name.chomp} перепил и скончался.\n")
+  describe '.a_lot_of_mana' do
+    subject do
+      human.mana = 100
+      cause.a_lot_of_mana(human)
+    end
+
+    it { is_expected.to eq("#{human.name.chomp} перепил и скончался.\n") }
   end
 
-  it '.less_happiness' do
-    human.happiness = -10
-    expect(cause.less_happiness(human)).to eq("#{human.name.chomp} скончался от непреодалимой грусти.\n")
+  describe '.less_happiness' do
+    subject do
+      human.happiness = -10
+      cause.less_happiness(human)
+    end
+
+    it { is_expected.to eq("#{human.name.chomp} скончался от непреодалимой грусти.\n") }
   end
 
-  it '.in_arrears' do
-    human.money = -50
-    expect(cause.in_arrears(human)).to eq("#{human.name.chomp} забили за долги.\n")
+  describe '.in_arrears' do
+    subject do
+      human.money = -50
+      cause.in_arrears(human)
+    end
+
+    it { is_expected.to eq("#{human.name.chomp} забили за долги.\n") }
   end
 
-  it '.get_cause_of_dead' do
-    human.money = -50
-    expect(cause.get_cause_of_dead(human)).to eq("#{human.name.chomp} забили за долги.\n")
+  describe '.get_cause_of_dead' do
+    subject do
+      human.money = -50
+      cause.get_cause_of_dead(human)
+    end
+
+    it { is_expected.to eq("#{human.name.chomp} забили за долги.\n") }
   end
 end
